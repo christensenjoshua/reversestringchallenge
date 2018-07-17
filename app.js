b5462ffb9b99192e0e3f8121d3a7bf83f1357a68
christensenjoshua <christensen.joshua@outlook.com>
2018-07-16 18:12:41 -0600

    display to paragraph

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ function reverseString(word) {
     for (nl = 0; nl <= word.length; nl++) {
         newWord = word.charAt(nl) + newWord
     }
-    console.log(newWord)
+    document.getElementById(reversedString).innerText = newWord
 }
