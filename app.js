f28dbe34c43a2bcce451142689b6531a5db79be7
christensenjoshua <christensen.joshua@outlook.com>
2018-07-16 18:17:00 -0600

    corrected extra for loop, finished

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var word = 'Hello'
 var newWord = ''
 reverseString(word)
 function reverseString(word) {
-    for (nl = 0; nl <= word.length; nl++) {
+    for (nl = 0; nl < word.length; nl++) {
         newWord = word.charAt(nl) + newWord
     }
     document.getElementById('reversedString').innerText = newWord
